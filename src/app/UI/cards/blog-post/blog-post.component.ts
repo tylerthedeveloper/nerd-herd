29223c263ee47c3599260a664b3a176072ff845a
Tyler Citrin <tcitrin@indiana.edu>
2017-06-18 15:09:23 -0400

    organizing wall GUI

---
+++ src/app/UI/cards/blog-post/blog-post.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'blog-post',
+  templateUrl: './blog-post.html',
+})
+export class BlogPostComponent {
+  //title = 'Contact us';
+  //bgImageUrl = 'https://demot-vertigostudio.netdna-ssl.com/hestia/wp-content/themes/hestia/assets/img/header.jpg';
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,11 @@ import { NavBarComponent }  from './UI/nav/navbar.component';
 import { PageHeaderComponent }  from './UI/templates/page-header/page-header.component';
 import { SubscribeComponent }  from './UI/templates/subscribe/subscribe.component';
 import { ProfileBoxComponent }  from './UI/cards/profile-box/profile-box.component';
-
+import { BlogPostComponent }  from './UI/cards/blog-post/blog-post.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent, NavBarComponent, ProfileBoxComponent,
+  declarations: [ AppComponent, NavBarComponent, ProfileBoxComponent,BlogPostComponent,
                   SubscribeComponent, ContactComponent, PageHeaderComponent ],
   bootstrap:    [ AppComponent ]
 })
