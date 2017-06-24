1713bd153898e5a2b6d7571e86a88395fa6a8c63
Tyler Citrin <tcitrin@indiana.edu>
2017-06-24 14:34:27 -0400

    some angular 4 updates: working ng-serve on angular/cli + compiler

--- src/app/UI/nav/navbar.component.ts
+++ src/app/UI/nav/navbar.component.ts
@@ -4,7 +4,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'nav-bar',
   templateUrl: './navbar.html',
-  styleUrls: ['/../app.component.css'],
+ // styleUrls: ['/../app.component.css']
 })
 export class NavBarComponent {
   navx ='<sign-up></sign-up>';  
