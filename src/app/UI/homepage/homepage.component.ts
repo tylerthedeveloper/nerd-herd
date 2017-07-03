8a519f3202286b8961f1717a31cd4d7b169097c8
Tyler Citrin <tcitrin@indiana.edu>
2017-07-03 17:02:29 -0400

    sampling auth + users in db

--- src/app/UI/homepage/homepage.component.ts
+++ src/app/UI/homepage/homepage.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { AuthService } from '../../_services/auth.service';
-//import { AngularFireDatabase, FirebaseObjectObservable, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireDatabase, FirebaseObjectObservable, FirebaseListObservable } from 'angularfire2/database';
 //import { Subject } from 'rxjs/Subject';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
@@ -13,14 +13,29 @@ import * as firebase from 'firebase/app';
 
 export class HomeComponent {
   
+  //users: FirebaseListObservable<any>;
   user: Observable<firebase.User>;
+  usersRef = firebase.database().ref('users');
+  rootDbRef = firebase.database().ref();
 
-  constructor(public afAuth: AngularFireAuth) {
+  constructor(public afAuth: AngularFireAuth, db: AngularFireDatabase) {
     this.user = afAuth.authState;
+    //this.users = db.list('/users');
   }
 
   login() {
-    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(function (result) {
+        var token = result.credential.accessToken; // This gives you a Google Access Token. 
+        var user = result.user; // The signed-in user info.      
+        
+        firebase.database().ref('/users/' + user.uid).set({
+          name: user.displayName,
+          email: user.email,
+        });
+    }).catch(function (error) {
+      //var errorCode = error.code;
+      var errorMessage = error.message;
+     });
   }
 
   logout() {
