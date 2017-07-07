f07ffb97e8d52fe6f1a5e51bc1d62811da8d1d28
tyler <tcitrin@indiana.edu>
2017-07-06 22:43:57 -0400

    profile redirect after login

--- src/app/UI/nav/navbar.component.ts
+++ src/app/UI/nav/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AFService } from '../../_services/af.service';
 import { Observable } from 'rxjs/Observable';
+import { Router } from "@angular/router";
 import * as firebase from 'firebase/app';
 
 @Component({
@@ -14,26 +15,23 @@ export class NavBarComponent {
   navx ='<sign-up></sign-up>';  
   loggedIn : boolean;
   user: firebase.User;
-  constructor(public afService : AFService) {
-    this.afService.afAuth.authState.subscribe(
+  constructor(public afService : AFService, private router: Router) {
+    this.afService.getUser().subscribe(
       (auth) => {
-        if (auth != null) {
-          //this.user = db.list.object('users/' + auth.uid);
-          this.loggedIn = true;
-        }
-        else 
-          this.loggedIn = false;        
+        this.user = auth;
+        if (auth != null) this.loggedIn = true;
+        else this.loggedIn = false;        
       });  
   }
   
-  login() {
-    this.afService.loginWithGoogle();
-    //this.loggedIn = true;
+  // call auth service to logout
+  login() : firebase.Promise<any> {
+    return this.afService.loginWithGoogle(); 
   }
 
-  logout() {
-    this.afService.logout();
-    //this.loggedIn = false;
+  // call auth service to logout
+  logout() : firebase.Promise<any> {
+    return this.afService.logout();
   }
 
 }
--- src/app/_services/af.service.ts
+++ src/app/_services/af.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { Router } from "@angular/router";
 import * as firebase from 'firebase/app';
 
 @Injectable()
@@ -8,34 +9,45 @@ export class AFService {
   
   user: Observable<firebase.User>;
 
-  constructor(public afAuth: AngularFireAuth) {
+  constructor(public afAuth: AngularFireAuth, private router: Router) {
     this.user = afAuth.authState;
   }
 
-  getUser() {
-      return this.afAuth.authState;
+  getUser(): Observable<firebase.User> {
+      return this.user;
   }
 
-  loginWithGoogle() {
-    return this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(function (result) {
-        var token = result.credential.accessToken; // This gives you a Google Access Token. 
-        var user = result.user; // The signed-in user info.      
-        //this.addUser(user);
-        firebase.database().ref('users/' + user.uid).set({ // add to db
-          name: user.displayName,          
-          email: user.email,
-          photoUrl: user.photoURL,
-          uid: user.uid
+  //login user with google auth on firebase
+  loginWithGoogle() : firebase.Promise<any> {
+      return this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider()).then( (result) => {
+          var token = result.credential.accessToken; // This gives you a Google Access Token. 
+          var user = result.user; // The signed-in user info.      
+          this.addUser(user);
+          /* firebase.database().ref('users/' + user.uid).set({ 
+              name: user.displayName,          
+              email: user.email,
+              photoUrl: user.photoURL,
+              uid: user.uid }); */
+          this.router.navigate(['/profile', user.uid]);
+      }).catch(function (error) {
+        //var errorCode = error.code;
+        var errorMessage = error.message;
+        alert( errorMessage );
       });
-    }).catch(function (error) {
-      //var errorCode = error.code;
-      var errorMessage = error.message;
-      alert( errorMessage );
-     });
-  }
+    }
 
-  logout() {
-    return this.afAuth.auth.signOut();
-  }
+    
+    logout() : firebase.Promise<any> {
+      return this.afAuth.auth.signOut();
+    }
 
+    // add or update user to db
+    private addUser(user: any) : void {
+        firebase.database().ref('users/' + user.uid).set({ // add to db
+            name: user.displayName,          
+            email: user.email,
+            photoUrl: user.photoURL,
+            uid: user.uid
+        });
+    }
 }
