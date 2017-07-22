c65bbabb8fd46c90e73964fd73ae980b91dff0da
Jack Garlick <jagarlic@umail.iu.edu>
2017-07-22 00:26:31 -0400

    major UI chages

---
+++ src/app/UI/components/index.ts
@@ -0,0 +1,2 @@
+export * from './profile-box/profile-box.component';
+export * from './friends-list/friends-list.component';
--- src/app/ng-md.module.ts
+++ src/app/ng-md.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import { MdButtonModule,  MdTabsModule, MdMenuModule, MdCardModule, MdInputModule } from '@angular/material';
+import { MdButtonModule,  MdTabsModule, MdMenuModule, MdCardModule, MdInputModule, MdGridListModule, MdTableModule, MdSidenavModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [ BrowserAnimationsModule, MdButtonModule, MdTabsModule, MdMenuModule, MdCardModule, MdInputModule ],
-  exports: [ BrowserAnimationsModule, MdButtonModule, MdTabsModule, MdMenuModule, MdCardModule, MdInputModule ],
+  imports: [ BrowserAnimationsModule, MdButtonModule, MdTabsModule, MdMenuModule, MdCardModule, MdInputModule, MdGridListModule, MdTableModule, MdSidenavModule ],
+  exports: [ BrowserAnimationsModule, MdButtonModule, MdTabsModule, MdMenuModule, MdCardModule, MdInputModule, MdGridListModule, MdTableModule, MdSidenavModule ],
 })
 export class MaterialDesignModule { }
