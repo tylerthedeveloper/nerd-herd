9b41465750b03bdce391eea1edd342e5aac1f29c
Jack Garlick <jagarlic@umail.iu.edu>
2017-08-05 01:11:59 -0400

    More UI changes, timestamps

--- src/app/UI/_feeds/post-feed/post-feed.component.ts
+++ src/app/UI/_feeds/post-feed/post-feed.component.ts
@@ -6,16 +6,38 @@ import { Post } from '../../../_models/post';
 import { Subject } from 'rxjs/Subject';
 
 @Component({
-  selector: 'post-feed',
-  templateUrl: './post-feed.html',
-  providers: [ PostService ],
-  styleUrls: ['./post-feed.css']
+    selector: 'post-feed',
+    templateUrl: './post-feed.html',
+    providers: [ PostService ],
+    styleUrls: ['./post-feed.css']
 })
 
 export class PostFeedComponent {
 
     posts : Observable<Post[]>;
-    private subject = new Subject<any>();
+    private subject = new Subject<any>();
+    postTypes = [
+        {value: 'other-0', viewValue: 'Other'},
+        {value: 'question-0', viewValue: 'Question'},
+        {value: 'interview-1', viewValue: 'Interview Prep'},
+        {value: 'social-2', viewValue: 'Social'},
+        {value: 'conference-3', viewValue: 'Conference'}
+    ];
+    postTypeSearch = [
+        {value: 'other-0', viewValue: 'All'},
+        {value: 'question-0', viewValue: 'Question'},
+        {value: 'interview-1', viewValue: 'Interview Prep'},
+        {value: 'social-2', viewValue: 'Social'},
+        {value: 'conference-3', viewValue: 'Conference'}
+    ];
+    postRadiusSearch = [
+        {value: 'radius-0', viewValue: '10 Miles'},
+        {value: 'radius-1', viewValue: '20 Miles'},
+        {value: 'radius-2', viewValue: '30 Miles'},
+        {value: 'radius-3', viewValue: '50 Miles'},
+        {value: 'radius-4', viewValue: '100 Miles'},
+        {value: 'radius-5', viewValue: 'All'}
+    ];
 
     constructor(private postService: PostService) { }
 
--- src/app/ng-md.module.ts
+++ src/app/ng-md.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import { MdButtonModule,  MdTabsModule, MdMenuModule, MdCardModule, MdInputModule, MdGridListModule, MdTableModule, MdSidenavModule } from '@angular/material';
+import { MdButtonModule,  MdTabsModule, MdMenuModule, MdCardModule, MdInputModule, MdGridListModule, MdTableModule, MdSidenavModule, MdSelectModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [ BrowserAnimationsModule, MdButtonModule, MdTabsModule, MdMenuModule, MdCardModule, MdInputModule, MdGridListModule, MdTableModule, MdSidenavModule ],
-  exports: [ BrowserAnimationsModule, MdButtonModule, MdTabsModule, MdMenuModule, MdCardModule, MdInputModule, MdGridListModule, MdTableModule, MdSidenavModule ],
+  imports: [ BrowserAnimationsModule, MdButtonModule, MdTabsModule, MdMenuModule, MdCardModule, MdInputModule, MdGridListModule, MdTableModule, MdSidenavModule, MdSelectModule ],
+  exports: [ BrowserAnimationsModule, MdButtonModule, MdTabsModule, MdMenuModule, MdCardModule, MdInputModule, MdGridListModule, MdTableModule, MdSidenavModule, MdSelectModule ],
 })
 export class MaterialDesignModule { }
